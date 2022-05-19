a165b0ddb60fcf763697e2edb45ddecdde62a03d
AmitMendl <amit@mendelevitch@gmail.com>
2022-05-19 22:52:20 +0300

    working on css :(

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {newMatrix, useInput, tileMatrix } from './GridController'
 import './Grid.css'
+import './SizeSelect.css'
       
 const Grid = (props) => {
 
@@ -12,20 +13,43 @@ const Grid = (props) => {
   
   useInput(Matrix, setMatrix, score, setScore);
 
+  const Reset = () => { //Reset Button
+    return (
+      <button className='Reset' onClick={() => {setMatrix(newMatrix(width, height));setScore(0);}}>
+        RESET
+      </button>)
+  }
+
+  const Score = () => {
+    return (
+    <div className='Score'>
+      <div className='ScoreText'>
+        {score}
+      </div>
+    </div>
+    )
+  }
+
   return (
     <div className='Container'>
       <div className='GameHeader'>
-        <button className='Reset' onClick={() => {
-            setMatrix(newMatrix(width, height))
-            setScore(0);
-          }}>
-            
-            RESET
-        </button>
-        <div className='Score'>
-          <div className='ScoreText'>
-            {score}
-          </div>
+        <Reset/>
+        <Score/>
+        <div className='SizeHeader'>
+          <select>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+          </select>
+          <select>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+          </select>
         </div>
       </div>
       <div>
